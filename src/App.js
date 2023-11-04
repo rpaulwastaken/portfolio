9735baab69d486ce7a12519ac8a89b1af6d63b06
rpaulwastaken <136478346+rpaulwastaken@users.noreply.github.com>
2023-11-04 23:22:09 +0530

    Updated Website

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Navbar from "./components/navbar";
 import IBox from "./components/intro_box";
 import IText from "./components/intro_text";
 import Avatar from "./components/avatar";
+import Skills from "./components/skills";
 import "./App.css";
 
 function App() {
   return (
       <>
           <div className={"bgColor"} />
+          <div> <Navbar/> </div>
           <div> <Avatar /></div>
           <div> <IText /></div>
           <div> <IBox /></div>
-          <div> <Navbar/> </div>
+          <div> <Skills /></div>
       </>
   );
 }
